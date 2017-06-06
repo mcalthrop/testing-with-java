e4631465107f907156f2c5400968243d15d7d1fd
Matt Calthrop <mcalthrop@gmail.com>
2017-06-06 17:32:41 +0100

    Update run-tests.bash scripts to work on Windows

--- step-2-tdd-fibonacci-junit/run-tests.bash
+++ step-2-tdd-fibonacci-junit/run-tests.bash
@@ -1,6 +1,10 @@
 #!/bin/bash -ex
 
-export CLASSPATH=.:src:test:test/junit-4.12.jar:test/hamcrest-core-1.3.jar
+PWD=$(pwd)
+# NOTE: the following env var is for use with git bash on Windows installations only,
+# and will vary according to the version of the JDK you installed:
+export PATH="${PATH}:/c/Program Files/Java/jdk1.8.0_131/bin"
+export CLASSPATH=.:${PWD}/src:${PWD}/test:test/junit-4.12.jar:test/hamcrest-core-1.3.jar
 
 javac src/Fibonacci.java
 javac test/FibonacciTest.java
