155737801feeecd47d6962f6373eaf1664deb274
Matt Calthrop <mcalthrop@gmail.com>
2017-06-07 12:25:36 +0100

    add test for illegal argument exception

--- step-1-junit/test/FactorialTest.java
+++ step-1-junit/test/FactorialTest.java
@@ -33,4 +33,9 @@ public class FactorialTest {
 
         assertEquals(expectedResult, factorial.calculate(testStartValue));
     }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void calculate_negative() {
+        factorial.calculate(-5);
+    }
 }
