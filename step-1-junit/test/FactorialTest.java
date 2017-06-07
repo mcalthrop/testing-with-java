ceec3fba7de4f7901300f2d6c178439215739bc4
Matt Calthrop <mcalthrop@gmail.com>
2017-06-07 11:36:21 +0100

    create first factorial test

--- step-1-junit/test/FactorialTest.java
+++ step-1-junit/test/FactorialTest.java
@@ -1,2 +1,13 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class FactorialTest {
+    @Test
+    public void calculate_0() {
+        Factorial factorial = new Factorial();
+        int testStartValue = 0;
+        int expectedResult = 0;
+
+        assertEquals(expectedResult, factorial.calculate(testStartValue));
+    }
 }
