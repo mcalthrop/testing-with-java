61a63d15760ad76e49401584f8c59ec0c8818247
Matt Calthrop <mcalthrop@gmail.com>
2017-06-07 15:19:38 +0100

    Update solution for Factorial.java

--- step-1-junit/src/Factorial.java
+++ step-1-junit/src/Factorial.java
@@ -1,2 +1,15 @@
 public class Factorial {
+    public int calculate(int start) {
+        if (start < 0) {
+            throw new IllegalArgumentException("Factorials are defined only on non-negative integers.");
+        }
+
+        int result = start;
+
+        if (result > 1) {
+            result = start * calculate(start - 1);
+        }
+
+        return result;
+    }
 }
