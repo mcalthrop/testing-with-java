5c87a16b17f86c36d00cc7cde2d688a3f1745dc4
Matt Calthrop <mcalthrop@gmail.com>
2017-06-07 11:52:31 +0100

    add second factorial test

--- step-1-junit/test/FactorialTest.java
+++ step-1-junit/test/FactorialTest.java
@@ -10,4 +10,13 @@ public class FactorialTest {
 
         assertEquals(expectedResult, factorial.calculate(testStartValue));
     }
+
+    @Test
+    public void calculate_1() {
+        Factorial factorial = new Factorial();
+        int testStartValue = 1;
+        int expectedResult = 1;
+
+        assertEquals(expectedResult, factorial.calculate(testStartValue));
+    }
 }
