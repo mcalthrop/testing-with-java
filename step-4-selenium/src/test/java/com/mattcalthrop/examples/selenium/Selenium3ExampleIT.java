7fa0968ded3253c866fe1c207124836d486b2a47
Matt Calthrop <mcalthrop@gmail.com>
2017-06-11 21:10:45 +0100

    Update solution test class

--- step-4-selenium/src/test/java/com/mattcalthrop/examples/selenium/Selenium3ExampleIT.java
+++ step-4-selenium/src/test/java/com/mattcalthrop/examples/selenium/Selenium3ExampleIT.java
@@ -18,4 +18,41 @@ public class Selenium3ExampleIT {
 
     private Boolean result;
 
+    @BeforeClass
+    public static void beforeAll() {
+        startUrl = "http://www.google.co.uk/";
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @AfterClass
+    public static void afterAll() {
+        driver.close();
+        driver.quit();
+    }
+
+    @Before
+    public void beforeEach() {
+        driver.get(startUrl);
+    }
+
+    @Test
+    public void checkPageTitleOnInit() {
+        assertEquals("Google", driver.getTitle());
+    }
+
+    @Test
+    public void checkPageTitleAfterSearch() {
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.sendKeys("Sausages!");
+        searchField.submit();
+        // Google's search is rendered dynamically with JavaScript, so wait for the page to load.
+        result = wait.until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                return driver.getTitle().toLowerCase().startsWith("sausages!");
+            }
+        });
+        assertTrue(result);
+    }
+
 }
