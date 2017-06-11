af7b9a6ebec0da3312b612b023e73daa30602898
Matt Calthrop <mcalthrop@gmail.com>
2017-06-11 20:07:54 +0100

    Update starter test class for step 4

--- step-4-selenium/src/test/java/com/mattcalthrop/examples/selenium/Selenium3ExampleIT.java
+++ step-4-selenium/src/test/java/com/mattcalthrop/examples/selenium/Selenium3ExampleIT.java
@@ -1,4 +1,21 @@
 package com.mattcalthrop.examples.selenium;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class Selenium3ExampleIT {
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+    private static String startUrl;
+
+    private Boolean result;
+
 }
